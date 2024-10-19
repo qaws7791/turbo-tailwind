ab5b1b190c66c3d01f706266d66ba13e7d061611
qaws7791 <qaws7791@gmail.com>
2024-10-19 22:49:20 +0900

    feat: dayjs를 사용하여 스케줄 생성

--- apps/api/src/events/events.service.ts
+++ apps/api/src/events/events.service.ts
@@ -4,6 +4,24 @@ import { Event, EventOperationTime } from './entities/event.entity';
 import { Repository } from 'typeorm';
 import { CreateEventDto } from './dto/create-event.dto';
 import { FindScheduleDto } from './dto/find-schedule.dto';
+import * as dayjs from 'dayjs';
+import * as isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import * as isBetween from 'dayjs/plugin/isBetween';
+import * as customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isBetween);
+dayjs.extend(customParseFormat);
+
+const dayOfWeekMap = {
+  0: 'sun',
+  1: 'mon',
+  2: 'tue',
+  3: 'wed',
+  4: 'thu',
+  5: 'fri',
+  6: 'sat',
+};
 
 @Injectable()
 export class EventsService {
@@ -52,12 +70,60 @@ export class EventsService {
   }
 
   // 입력받은 시작 시간과 끝 시간 사이에 있는 예약 가능한 스케쥴 배열을 생성하는 메서드를 구현합니다.
-  findSchedules(id: number, findScheduleDto: FindScheduleDto) {
-    const event = this.eventRepository.findOne({
+  async findSchedules(id: number, findScheduleDto: FindScheduleDto) {
+    const event = await this.eventRepository.findOne({
       where: { id },
       relations: ['operationTimes'],
     });
 
+    const { unitBookingTime } = event;
     const { startDateTime, endDateTime } = findScheduleDto;
+
+    const start = dayjs(startDateTime);
+    const end = dayjs(endDateTime);
+
+    const availableSlots = [];
+
+    let currentDate = start;
+    while (currentDate.isSameOrBefore(end)) {
+      const dayOfWeek = dayOfWeekMap[currentDate.day()];
+      const operationTime = event.isUniformOperationTime
+        ? event.operationTimes[0]
+        : event.operationTimes.find(
+            (operationTime) => operationTime.day === dayOfWeek,
+          );
+
+      if (operationTime) {
+        const openTime = dayjs(operationTime.startTime, 'HH:mm:ss');
+        const closeTime = dayjs(operationTime.endTime, 'HH:mm:ss');
+        let slotStart = currentDate
+          .hour(openTime.hour())
+          .minute(openTime.minute())
+          .second(0);
+        while (
+          slotStart.isBefore(
+            currentDate.hour(closeTime.hour()).minute(closeTime.minute()),
+          )
+        ) {
+          const slotEnd = slotStart.add(unitBookingTime, 'minute');
+          if (
+            slotEnd.isSameOrBefore(
+              currentDate.hour(closeTime.hour()).minute(closeTime.minute()),
+            )
+          ) {
+            availableSlots.push({
+              date: currentDate.format('YYYY-MM-DD'),
+              time: slotStart.format('HH:mm'),
+              endTime: slotEnd.format('HH:mm'),
+            });
+          }
+          slotStart = slotEnd;
+        }
+      }
+
+      currentDate = currentDate.add(1, 'day');
+    }
+
+    return availableSlots;
   }
 }
