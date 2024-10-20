579db5a31bd2105a2579aac899585e4cf2b37f00
qaws7791 <qaws7791@gmail.com>
2024-10-20 23:53:42 +0900

    feat: tailwind prefix 추가

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,11 +1,12 @@
-import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
+import type { Config } from "tailwindcss";
 import reactAriaPlugin from "tailwindcss-react-aria-components";
 
 const config: Pick<Config, "prefix" | "presets" | "content" | "plugins"> = {
   content: ["./src/**/*.tsx"],
   presets: [sharedConfig],
   plugins: [reactAriaPlugin],
+  prefix: "ui-",
 };
 
 export default config;
