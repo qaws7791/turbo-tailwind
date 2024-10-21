af6e6dd02c0f648ec85a728e1585a7e522349a57
qaws7791 <qaws7791@gmail.com>
2024-10-22 03:22:41 +0900

    feat: Input 컴포넌트 추가

---
+++ apps/docs/stories/Input.stories.tsx
@@ -0,0 +1,63 @@
+import { Input } from "@repo/ui/input";
+import { Meta, StoryObj } from "@storybook/react";
+import { Mail } from "lucide-react";
+
+const meta = {
+  title: "Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    placeholder: "Type something...",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => <Input {...args} />,
+};
+
+export const Disabled: Story = {
+  render: (args) => <Input {...args} disabled />,
+};
+
+export const File: Story = {
+  render: (args) => <Input {...args} type="file" />,
+};
+
+export const Invalid: Story = {
+  render: (args) => <Input {...args} type="email" aria-invalid />,
+};
+
+export const WithIcon: Story = {
+  render: (args) => (
+    <div className="flex flex-col gap-2">
+      <div className="relative">
+        <Input {...args} className="pr-10" />
+        <Mail className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 size-5" />
+      </div>
+      <div className="relative">
+        <Input {...args} className="pl-10" />
+        <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 size-5" />
+      </div>
+    </div>
+  ),
+};
+
+export const WithUnit: Story = {
+  render: (args) => (
+    <div className="relative">
+      <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-sm">
+        ₩
+      </span>
+      <Input {...args} className="pr-12 pl-7" placeholder="Type amount..." />
+      <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-sm">
+        KRW
+      </span>
+    </div>
+  ),
+};
---
+++ packages/ui/src/input.tsx
@@ -0,0 +1,18 @@
+import { forwardRef } from "react";
+import { tv } from "tailwind-variants";
+import { cn, focusRing } from "./utils";
+
+export const inputStyles = tv({
+  extend: focusRing,
+  base: "u-w-full u-h-10 u-bg-transparent placeholder:u-text-gray-400 u-text-sm u-border u-border-gray-400 u-rounded-lg u-pl-3 u-pr-3 u-py-2 u-transition u-duration-300 u-ease shadow-sm focus:shadow file:u-border-0 file:u-bg-transparent aria-invalid:u-border-red-500 disabled:u-bg-gray-100 disabled:u-text-gray-400 disabled:u-cursor-not-allowed",
+});
+
+export type InputProps = React.ComponentPropsWithRef<"input">;
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input className={cn(inputStyles({ className }))} ref={ref} {...props} />
+    );
+  }
+);
