ea6c59209e9e3480a43aa52225de261058bc9311
qaws7791 <qaws7791@gmail.com>
2024-10-22 03:21:43 +0900

    fix: 클래스 적용 순서를 위해 globals.css를 마지막에 가져오기

--- apps/docs/.storybook/preview.ts
+++ apps/docs/.storybook/preview.ts
@@ -1,7 +1,7 @@
-import type { Preview } from "@storybook/react";
-import "../src/globals.css";
-//
 import "@repo/ui/styles.css";
+import type { Preview } from "@storybook/react";
+// 마지막에 import
+import "../src/globals.css";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
