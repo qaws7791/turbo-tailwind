13f774046cfe142a4d028bd485fe94aacebe0fcf
qaws7791 <qaws7791@gmail.com>
2024-10-22 08:33:38 +0900

    feat: font pretendard로 변경

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,11 +2,23 @@ import sharedConfig from "@repo/tailwind-config";
 import type { Config } from "tailwindcss";
 import animatePlugin from "tailwindcss-animate";
 
-const config: Pick<Config, "prefix" | "presets" | "content" | "plugins"> = {
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const config: Config = {
   content: ["./src/**/*.tsx", "./src/**/*.ts"],
   presets: [sharedConfig],
   plugins: [animatePlugin],
   prefix: "u-",
+  corePlugins: {
+    preflight: true,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 };
 
 export default config;
