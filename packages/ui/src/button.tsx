94a1c99fa14e8159cd00dfa308a48117b07510ab
qaws7791 <qaws7791@gmail.com>
2024-10-21 19:28:32 +0900

    feat: button 컴포넌트 추가

---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,60 @@
+import { Slot } from "@radix-ui/react-slot";
+import React, { forwardRef } from "react";
+import type { VariantProps } from "tailwind-variants";
+import { tv } from "tailwind-variants";
+import type { RadixUISlot } from "./types";
+import { cn, focusRing } from "./utils";
+
+export const buttonStyles = tv({
+  extend: focusRing,
+  base: "u-font-medium u-rounded-xl u-cursor-pointer u-flex u-items-center u-justify-center u-transition-all disabled:u-opacity-50 disabled:u-cursor-not-allowed u-whitespace-nowrap",
+  variants: {
+    variant: {
+      primary:
+        "u-bg-blue-500 u-text-white hover:u-bg-blue-600 active:u-bg-blue-700",
+      secondary:
+        "u-bg-purple-500 u-text-white hover:u-bg-purple-600 active:u-bg-purple-700",
+      success:
+        "u-bg-green-500 u-text-white hover:u-bg-green-600 active:u-bg-green-700",
+      danger:
+        "u-bg-red-500 u-text-white hover:u-bg-red-600 active:u-bg-red-700",
+      outline:
+        "u-bg-transparent u-text-blue-600 u-border-blue-600 u-border-2 u-box-border hover:u-bg-blue-100 active:u-bg-blue-200",
+      ghost:
+        "u-bg-transparent u-text-blue-600 hover:u-bg-blue-100 active:u-bg-blue-200",
+    },
+    size: {
+      sm: "u-text-sm u-px-3 u-py-2 u-h-9",
+      md: "u-text-base u-px-4 u-py-2 u-h-10",
+      lg: "u-px-4 u-py-2 u-text-lg u-h-11",
+    },
+  },
+  compoundVariants: [
+    {
+      size: ["sm", "md"],
+      class: "u-px-3 u-py-1",
+    },
+  ],
+  defaultVariants: {
+    size: "md",
+    variant: "primary",
+  },
+});
+
+export type ButtonProps = React.ComponentPropsWithRef<"button"> &
+  VariantProps<typeof buttonStyles> &
+  RadixUISlot;
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild, ...props }, ref) => {
+    const Component = asChild ? Slot : "button";
+    return (
+      <Component
+        className={cn(buttonStyles({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
