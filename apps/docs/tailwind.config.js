d0dd6e6c24116b00a6872dac8f2533a5adc6be33
qaws7791 <qaws7791@gmail.com>
2024-10-22 08:33:20 +0900

    feat: font 제거

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,15 +1,8 @@
 import sharedConfig from "@repo/tailwind-config";
-import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./stories/*.{ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Spoqa Han Sans Neo", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
+
   plugins: [],
   presets: [sharedConfig],
 };
