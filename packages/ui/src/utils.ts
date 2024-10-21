031168474c384a91d5499e34bab5ca1b33fc9d54
qaws7791 <qaws7791@gmail.com>
2024-10-21 19:27:32 +0900

    feat: focusRing 수정, 클래스 병합 용 cn 추가

--- packages/ui/src/utils.ts
+++ packages/ui/src/utils.ts
@@ -1,11 +1,12 @@
+import type { ClassValue } from "clsx";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 import { tv } from "tailwind-variants";
 
 export const focusRing = tv({
-  base: "outline outline-blue-600 dark:outline-blue-500 forced-colors:outline-[Highlight] outline-offset-2",
-  variants: {
-    isFocusVisible: {
-      false: "outline-0",
-      true: "outline-2",
-    },
-  },
+  base: "focus-visible:u-outline-none focus-visible:u-ring-2 focus-visible:u-ring-offset-2  focus-visible:u-ring-blue-500  u-ring-offset-white",
 });
+
+export const cn = (...inputs: ClassValue[]): string => {
+  return twMerge(clsx(inputs));
+};
