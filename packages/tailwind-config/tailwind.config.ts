d64b87b136e760b103cc8c9be3780569d6722c7c
qaws7791 <qaws7791@gmail.com>
2024-10-23 05:55:08 +0900

    feat: tailwind spacing 0.5 단위를 위한 설정 추가

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -11,6 +11,23 @@ const config: Omit<Config, "content"> = {
       aria: {
         invalid: "invalid=true",
       },
+      spacing: {
+        px: "1px",
+        ...Array.from({ length: 101 }, (_, i) => i / 2).reduce(
+          (acc, cur) => {
+            acc[cur] = `${cur * 4}px`;
+            return acc;
+          },
+          {} as Record<string, string>
+        ), //w-0 ~ w-50
+        52: "208px",
+        56: "224px",
+        60: "240px",
+        64: "256px",
+        72: "288px",
+        80: "320px",
+        96: "384px",
+      },
     },
   },
   plugins: [],
