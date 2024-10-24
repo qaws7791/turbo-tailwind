9c43b3e9e0cc29fb80294059b065f1d517ac7be5
qaws7791 <qaws7791@gmail.com>
2024-10-24 19:07:34 +0900

    feat: Switch 클릭 가능한 영역 증가

--- packages/ui/src/switch.tsx
+++ packages/ui/src/switch.tsx
@@ -8,7 +8,7 @@ import { cn, focusVisibleRing } from "./utils";
 
 export const switchStyles = tv({
   extend: focusVisibleRing,
-  base: "u-peer u-inline-flex u-h-6 u-w-11 u-shrink-0 u-cursor-pointer u-items-center u-rounded-full u-transition-colors disabled:u-cursor-not-allowed disabled:u-opacity-50 data-[state=checked]:u-bg-blue-500 data-[state=unchecked]:u-bg-gray-200",
+  base: "u-peer u-inline-flex u-h-6 u-w-11 u-shrink-0 u-cursor-pointer u-items-center u-rounded-full u-transition-colors disabled:u-cursor-not-allowed disabled:u-opacity-50 data-[state=checked]:u-bg-blue-500 data-[state=unchecked]:u-bg-gray-200 u-relative",
 });
 
 const Switch = React.forwardRef<
@@ -25,6 +25,7 @@ const Switch = React.forwardRef<
         "u-pointer-events-none u-block u-h-5 u-w-5 u-rounded-full u-bg-white u-shadow-lg u-ring-0 u-transition-transform data-[state=checked]:u-translate-x-5.5 data-[state=unchecked]:u-translate-x-0.5"
       )}
     />
+    <div className="u-absolute u-h-10 u-w-full" />
   </SwitchPrimitives.Root>
 ));
 Switch.displayName = SwitchPrimitives.Root.displayName;
