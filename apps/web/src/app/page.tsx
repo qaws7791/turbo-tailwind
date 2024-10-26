f9f4c2d4b3261c6ad0b47104c3bce2d5b18b0208
qaws7791 <qaws7791@gmail.com>
2024-10-26 22:12:01 +0900

    remove: <AppClient> 제거

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,7 @@
-import AppClient from "./app-client";
-
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <AppClient />
+      <h1>Home Page</h1>
     </main>
   );
 }
