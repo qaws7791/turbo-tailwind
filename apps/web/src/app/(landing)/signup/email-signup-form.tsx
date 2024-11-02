acd012837b9a91ab6811a92c91e2f92e1a32941e
qaws7791 <qaws7791@gmail.com>
2024-11-02 20:35:39 +0900

    feat: 이메일 회원가입 폼 및 페이지 컴포넌트 추가

---
+++ apps/web/src/app/(landing)/signup/email-signup-form.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { createClient } from "@/lib/supabase/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@repo/ui/button";
+import { ErrorMessage } from "@repo/ui/form";
+import { Input } from "@repo/ui/input";
+import { Label } from "@repo/ui/label";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8).max(30),
+  username: z.string().min(3).max(30),
+});
+
+type SignupData = z.infer<typeof signupSchema>;
+
+export default function EmailSignupForm() {
+  const router = useRouter();
+  const supabase = createClient();
+  const form = useForm<SignupData>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      username: "",
+    },
+  });
+
+  const onSubmit = form.handleSubmit(async (data: SignupData) => {
+    // Sign up the user
+    const { error } = await supabase.auth.signUp({
+      email: data.email,
+      password: data.password,
+      options: {
+        data: {
+          user_name: data.username,
+        },
+      },
+    });
+
+    if (error) {
+      form.setError("root", {
+        message: error.message,
+      });
+      return;
+    }
+
+    // Redirect to the home page
+    router.push("/app");
+  });
+
+  return (
+    <form className="flex flex-col gap-2" onSubmit={onSubmit}>
+      <Label>
+        Email
+        <Input className="mt-2" type="email" {...form.register("email")} />
+      </Label>
+      <ErrorMessage>{form.formState.errors.email?.message}</ErrorMessage>
+      <Label>
+        Password
+        <Input
+          className="mt-2"
+          type="password"
+          {...form.register("password")}
+        />
+      </Label>
+      <ErrorMessage>{form.formState.errors.password?.message}</ErrorMessage>
+      <Label>
+        Username
+        <Input className="mt-2" {...form.register("username")} />
+      </Label>
+      <ErrorMessage>{form.formState.errors.username?.message}</ErrorMessage>
+      <Button type="submit">Signup</Button>
+      <ErrorMessage>{form.formState.errors.root?.message}</ErrorMessage>
+    </form>
+  );
+}
---
+++ apps/web/src/app/(landing)/signup/page.tsx
@@ -0,0 +1,21 @@
+import EmailSignupForm from "@/app/(landing)/signup/email-signup-form";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function SignupPage() {
+  const supabase = createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (session) {
+    redirect("/app");
+  }
+  return (
+    <div>
+      <div className="max-w-xs mx-auto">
+        <h1 className="text-2xl font-semibold my-4">Login</h1>
+        <EmailSignupForm />
+      </div>
+    </div>
+  );
+}
