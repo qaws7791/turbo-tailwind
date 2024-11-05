fb4b8803181222a8eb3af334b34faae87b5aa7ec
qaws7791 <qaws7791@gmail.com>
2024-11-05 18:58:46 +0900

    feat: 리스트 상세 페이지에 링크 추가 버튼 및 링크 조회 컴포넌트 추가

--- apps/web/src/app/(app)/app/lists/[id]/page.tsx
+++ apps/web/src/app/(app)/app/lists/[id]/page.tsx
@@ -1,6 +1,16 @@
-import ListDeleteButton from "@/app/(app)/app/lists/[id]/list-delete-button";
-import ListEditButton from "@/app/(app)/app/lists/[id]/list-edit-button";
+import AddLinkButton from "@/components/links/add-link-button";
+import LinksView from "@/components/links/links-view";
+import ListDeleteButton from "@/components/lists/list-delete-button";
+import ListEditButton from "@/components/lists/list-edit-button";
+import { getLinks } from "@/lib/supabase/server/queries/links";
 import { getList } from "@/lib/supabase/server/queries/lists";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { Lock, UsersRound } from "lucide-react";
+import { Suspense } from "react";
 
 interface ListDetailPageProps {
   params: Promise<{ id: string }>;
@@ -11,15 +21,45 @@ export default async function ListDetailPage({ params }: ListDetailPageProps) {
 
   const list = await getList(id);
 
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["lists", id, "links"],
+    queryFn: () => {
+      return getLinks(id);
+    },
+  });
+
   return (
-    <div>
-      <h1 className="text-3xl font-medium">{list.title}</h1>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <div>
-        <p>{list.description}</p>
+        <div className="p-4">
+          <h1 className="text-3xl font-medium">{list.title}</h1>
+          <div>
+            <p>{list.description}</p>
+          </div>
+          <div className="flex items-center gap-1 text-sm">
+            {list.is_public ? (
+              <UsersRound className="size-4" />
+            ) : (
+              <Lock className="size-4" />
+            )}
+            {list.is_public ? "공개" : "비공개"}
+          </div>
+          <div className="mt-8 flex gap-2 justify-end">
+            <ListEditButton id={id} />
+            <ListDeleteButton id={id} />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between mt-4 p-4">
+          <h2 className="text-xl font-medium">Links</h2>
+          <AddLinkButton listId={id} />
+        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <LinksView listId={id} />
+        </Suspense>
       </div>
-      <p>{list.is_public ? "Public" : "Private"}</p>
-      <ListEditButton id={id} />
-      <ListDeleteButton id={id} />
-    </div>
+    </HydrationBoundary>
   );
 }
