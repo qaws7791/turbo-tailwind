5f6b3df54cb47b637f35d57d64087c811efcd43a
qaws7791 <qaws7791@gmail.com>
2024-11-05 18:57:18 +0900

    feat: createClient 함수를 비동기로 변경하여 로그아웃 기능 업데이트

--- apps/web/src/app/(api)/auth/signout/route.ts
+++ apps/web/src/app/(api)/auth/signout/route.ts
@@ -1,9 +1,9 @@
+import { createClient } from "@/lib/supabase/server";
 import { revalidatePath } from "next/cache";
 import { type NextRequest, NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/server";
 
 export async function POST(request: NextRequest) {
-  const supabase = createClient();
+  const supabase = await createClient();
 
   // Check if a user's logged in
   const {
