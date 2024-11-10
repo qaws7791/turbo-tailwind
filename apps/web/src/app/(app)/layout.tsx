f41a0c1f611e3daa6456a2b295de5ce4f2b7fe44
qaws7791 <qaws7791@gmail.com>
2024-11-10 21:02:30 +0900

    feat: 프로필 로딩 실패 시 로그인 페이지로 리다이렉트 처리 추가

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -4,41 +4,46 @@ import QueryProvider from "@/components/query-provider";
 import UserButton from "@/components/user-button";
 import getProfile from "@/lib/supabase/server/uesrs/users.queries";
 import { Toaster } from "@repo/ui/toaster";
+import { redirect } from "next/navigation";
 
 export default async function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }): Promise<JSX.Element> {
-  const profile = await getProfile();
+  try {
+    const profile = await getProfile();
 
-  if (!profile) {
-    return <div>loading...</div>;
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+
+    return (
+      <QueryProvider>
+        <div className="h-full w-full flex">
+          <div className="h-screen max-w-60 w-full p-4 border border-gray-200 hidden lg:flex lg:flex-col lg:justify-between">
+            <div>
+              <AppSideContent />
+            </div>
+            <div>
+              <UserButton
+                user={{
+                  name: profile.username,
+                  email: profile.email,
+                  avatar: profile.avatar_url,
+                }}
+              />
+            </div>
+          </div>
+          <div className="min-h-screen pt-16 lg:pt-0 w-full flex-1">
+            <AppHeader />
+            {children}
+          </div>
+          <Toaster />
+        </div>
+      </QueryProvider>
+    );
+  } catch (error) {
+    redirect("/login");
   }
-
-  return (
-    <QueryProvider>
-      <div className="h-full w-full flex">
-        <div className="h-screen max-w-60 w-full p-4 border border-gray-200 hidden lg:flex lg:flex-col lg:justify-between">
-          <div>
-            <AppSideContent />
-          </div>
-          <div>
-            <UserButton
-              user={{
-                name: profile.username,
-                email: profile.email,
-                avatar: profile.avatar_url,
-              }}
-            />
-          </div>
-        </div>
-        <div className="min-h-screen pt-16 lg:pt-0 w-full flex-1">
-          <AppHeader />
-          {children}
-        </div>
-        <Toaster />
-      </div>
-    </QueryProvider>
-  );
 }
