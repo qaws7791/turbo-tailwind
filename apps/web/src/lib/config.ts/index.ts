b36c2cf53ba23a50ed87d0288d9cd352abc43a6d
qaws7791 <qaws7791@gmail.com>
2024-12-02 03:36:41 +0900

    feat: 사이트 config에 한국어, 영어 분리

--- apps/web/src/lib/config.ts/index.ts
+++ apps/web/src/lib/config.ts/index.ts
@@ -1,7 +1,25 @@
 import { clientEnv } from "@/lib/env";
 
-export const siteConfig = {
-  siteName: "LinkBird",
-  siteDescription: "LinkBird is a links sharing platform",
+interface SiteConfig {
+  siteName: {
+    ko: string;
+    en: string;
+  };
+  siteDescription: {
+    ko: string;
+    en: string;
+  };
+  siteUrl: string;
+}
+
+export const siteConfig: SiteConfig = {
+  siteName: {
+    ko: "링크버드",
+    en: "LinkBird",
+  },
+  siteDescription: {
+    ko: "링크버드는 링크를 공유하고 관리하는 서비스입니다.",
+    en: "LinkBird is a service for sharing and managing links.",
+  },
   siteUrl: clientEnv.NEXT_PUBLIC_URL,
 } as const;
