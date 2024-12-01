53d287ab6e24740d5ff5f801d463816a6e00845a
qaws7791 <qaws7791@gmail.com>
2024-12-02 03:11:58 +0900

    feat: 랜딩 페이지에 설명 추가 및 시작하기 버튼 추가

--- apps/web/src/app/(public)/page.tsx
+++ apps/web/src/app/(public)/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@repo/ui/button";
+
 export default function LandingPage(): JSX.Element {
   return (
     <div>
       <section className="max-w-screen-xl mx-auto text-center mt-12 p-4">
-        <h1 className="text-blue-600 text-6xl font-semibold">리스트버드</h1>
-        <p className="text-5xl font-semibold mt-4">리스트 공유를 위한 플랫폼</p>
+        <h1 className="text-blue-600 text-6xl font-semibold">링크버드</h1>
+        <p className="text-5xl font-semibold mt-4">
+          링크들을 하나의 리스트에 모으고 공유하세요
+        </p>
+
+        <div className="mt-10">
+          <Button size="lg" className="mx-auto">
+            지금 시작하기
+          </Button>
+        </div>
       </section>
     </div>
   );
