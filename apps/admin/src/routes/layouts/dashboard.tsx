501ca0e05f0227038e6b50f4626c82ea03fc4fa5
qaws7791 <qaws7791@gmail.com>
2024-12-24 23:39:52 +0900

    feat: 대시보드 사이드바에 드로어 추가 및 레이아웃 개선

--- apps/admin/src/routes/layouts/dashboard.tsx
+++ apps/admin/src/routes/layouts/dashboard.tsx
@@ -1,9 +1,22 @@
 import {
+  Bars3Icon,
   HomeIcon,
   SquaresPlusIcon,
   TicketIcon,
   UsersIcon,
+  XMarkIcon,
 } from "@heroicons/react/20/solid";
+import { Button } from "@repo/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@repo/ui/drawer";
+import { IconButton } from "@repo/ui/icon-button";
 import { cn } from "@repo/ui/utils";
 import { Link, Outlet, useLocation } from "react-router";
 
@@ -30,21 +43,66 @@ const PAGES = [
   },
 ];
 
-export default function DashboardLayout() {
-  const location = useLocation();
-  console.log(location);
+function DashBoardSidebar() {
   return (
-    <div className="min-h-dvh flex bg-zinc-100">
-      <div className="w-64 p-2">
+    <>
+      <div className="flex items-center justify-between lg:hidden p-2">
+        <Drawer direction="left">
+          <DrawerTrigger asChild>
+            <IconButton variant="ghost">
+              <span className="sr-only">Open sidebar</span>
+              <Bars3Icon className="size-5" />
+            </IconButton>
+          </DrawerTrigger>
+          <DrawerContent direction="left">
+            <DrawerHeader>
+              <DrawerTitle className="sr-only">대시보드 사이드바</DrawerTitle>
+              <DrawerDescription className="sr-only">
+                원하는 페이지를 선택하세요.
+              </DrawerDescription>
+              <div>
+                <DrawerClose asChild>
+                  <IconButton variant="ghost">
+                    <XMarkIcon className="size-5" />
+                  </IconButton>
+                </DrawerClose>
+              </div>
+            </DrawerHeader>
+            <div>
+              <h1 className="font-semibold text-lg py-4 px-2">🧑‍💼대시보드</h1>
+              <nav className="mt-4">
+                <ul className="flex flex-col">
+                  {PAGES.map((page) => (
+                    <li key={page.path}>
+                      <Link
+                        to={page.path}
+                        className={cn(
+                          "px-2 py-2.5 rounded font-semibold flex text-zinc-500 hover:bg-zinc-200/70",
+                          location.pathname === page.path && "text-zinc-900"
+                        )}
+                      >
+                        {page.icon}
+                        <span className="ml-2">{page.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+          </DrawerContent>
+        </Drawer>
+        <Button>user</Button>
+      </div>
+      <div className="w-64 p-2 hidden lg:block">
         <h1 className="font-semibold text-lg py-4 px-2">🧑‍💼대시보드</h1>
         <nav className="mt-4">
-          <ul className="flex flex-col gap-2">
+          <ul className="flex flex-col">
             {PAGES.map((page) => (
               <li key={page.path}>
                 <Link
                   to={page.path}
                   className={cn(
-                    "p-2 rounded font-semibold flex text-zinc-500",
+                    "px-2 py-2.5 rounded font-semibold flex text-zinc-500 hover:bg-zinc-200/70",
                     location.pathname === page.path && "text-zinc-900"
                   )}
                 >
@@ -56,8 +114,18 @@ export default function DashboardLayout() {
           </ul>
         </nav>
       </div>
-      <main className="p-2 flex-1">
-        <div className="bg-white h-full rounded-xl p-4 border border-zinc-950/10">
+    </>
+  );
+}
+
+export default function DashboardLayout() {
+  const location = useLocation();
+  console.log(location);
+  return (
+    <div className="min-h-dvh flex lg:bg-zinc-100 flex-col lg:flex-row">
+      <DashBoardSidebar />
+      <main className="lg:p-2 flex-1">
+        <div className="bg-white h-full lg:rounded-xl p-4 lg:border border-zinc-950/10">
           <Outlet />
         </div>
       </main>
